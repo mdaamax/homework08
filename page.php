12d9057760b3bc648b82a25e2623919fb174ca17
mdaaamax <omgwtfmda@mail.ru>
2023-03-13 17:21:20 +0400

    Initial commit

---
+++ page.php
@@ -0,0 +1,60 @@
+<?php
+$Link1 = 'index.php';
+$Link2 = 'index2.php';
+$Link3 = 'index3.php';
+$Link4 = 'index4.php';
+$Link5 = 'index5.php';
+$Link6 = 'index6.php';
+$Link7 = 'index7.php';
+$Link8 = 'index8.php';
+$Link9 = 'index9.php';
+$Link10 = 'home1.php';
+$Link11 = 'home2.php';
+$Link12 = 'home3.php';
+$Link13 = 'page.php'; //для перехода на титульную страницу
+
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=style.css>
+    <title>Домашка</title>
+</head>
+<body>
+<div class="card">
+    <h1>Домашка</h1>
+    <h4>Занятие №8 </h4>
+    <a href="<?= $Link1 ?>">Упражнение №1</a>
+    <br>
+    <br>
+    <a href="<?= $Link2 ?>">Упражнение №2</a>
+    <br>
+    <br>
+    <a href="<?= $Link3 ?>">Упражнение №3</a>
+    <br>
+    <br>
+    <a href="<?= $Link4 ?>">Упражнение №4</a>
+    <br>
+    <br>
+    <a href="<?= $Link5 ?>">Упражнение №5</a>
+    <br>
+    <br>
+    <a href="<?= $Link10 ?>">Домашняя работа №1</a>
+    <br>
+    <br>
+    <a href="<?= $Link11 ?>">Домашняя работа №2</a>
+    <br>
+    <br>
+    <a href="<?= $Link12 ?>">Домашняя работа №3</a>
+
+
+
+
+
+</div>
+</body>
+</html>
